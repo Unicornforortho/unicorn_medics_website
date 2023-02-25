cf46ccd2e08b10faf0786f85c4c3f87f2c09de0c
Aditya Nagar <nagar.s.aditya@gmail.com>
2023-02-25 21:32:37 +0530

    feat: add join-us page

---
+++ pages/join-us/index.tsx
@@ -0,0 +1,20 @@
+import { Text, Anchor, Box } from '@mantine/core';
+
+export default function JoinUs() {
+  return (
+    <Box>
+      <Text fz={48} fw={700}>
+        We are full right now :(
+      </Text>
+      <Text>
+        Thank you for your interest in joining us. At the moment we don’t have any vacancies. Please
+        look at this page regularly for opportunities. If you have any general queries or
+        suggestions please feel free to{' '}
+        <Anchor<'a'> href="/contact-us" type="text">
+          contact us
+        </Anchor>{' '}
+        to discuss further.
+      </Text>
+    </Box>
+  );
+}
