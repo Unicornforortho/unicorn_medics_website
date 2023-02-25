5326f53e07623089c902f55c63bf2edcc367d04c
Darshil Mehta <darshilm223@gmail.com>
2023-02-25 20:29:40 +0530

    feat: add contactUs SMTP server credentials and sendEmail methods

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import { useState } from 'react';
 import '../style/styles.css';
 import NextApp, { AppProps, AppContext } from 'next/app';
@@ -27,6 +28,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         <title>Impro</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="shortcut icon" href="/favicon.svg" />
+        <script src="http://smtpjs.com/v3/smtp.js" />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
--- pages/contact-us/index.tsx
+++ pages/contact-us/index.tsx
@@ -1,5 +1,7 @@
 import { TextInput, Textarea, SimpleGrid, Group, Text, Button, Container } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { showNotification } from '@mantine/notifications';
+import { IconAlertCircle } from '@tabler/icons';
 
 export default function GetInTouchSimple() {
   const form = useForm({
@@ -16,8 +18,40 @@ export default function GetInTouchSimple() {
     },
   });
 
+  const handleSumbit = () => {
+    Email.send({
+      Host: 'smtp.elasticemail.com',
+      Port: 2525,
+      Username: process.env.NEXT_PUBLIC_STMP_USERNAME,
+      Password: process.env.NEXT_PUBLIC_STMP_PASSWORD,
+      To: 'iimpro.contact.us@gmail.com',
+      From: process.env.NEXT_PUBLIC_STMP_USERNAME,
+      Subject: `${form.values.email} - ${form.values.subject}`,
+      Body: form.values.message,
+    })
+      .then(() => {
+        form.reset();
+        showNotification({
+          title: 'Message sent',
+          message: 'Thank you for contacting us, we will get back to you as soon as possible.',
+          color: 'teal',
+          autoClose: 5000,
+        });
+      })
+      .catch(() => {
+        form.reset();
+        showNotification({
+          title: 'Internal Server Error',
+          message: 'Your message could not be sent, please try again later.',
+          color: 'red',
+          autoClose: 5000,
+          icon: <IconAlertCircle />,
+        });
+      });
+  };
+
   return (
-    <form onSubmit={form.onSubmit(() => {})}>
+    <form onSubmit={form.onSubmit(() => handleSumbit())}>
       <Text fz={48} fw={500} mb={15} ta="center">
         Get in touch
       </Text>
@@ -57,7 +91,7 @@ export default function GetInTouchSimple() {
           autosize
           name="message"
           variant="filled"
-          {...form.getInputProps('subject')}
+          {...form.getInputProps('message')}
         />
 
         <Group position="center" mt="xl">
